f4b5b4c625cd668ba0ce803307fb932e3dd90c39
dzenang <dzenan.granulo@gmail.com>
2024-10-08 13:46:58 +0200

    Add additional exception handler in controller advice

--- src/main/java/com/example/springdatajpa/exception/OurExceptionHandler.java
+++ src/main/java/com/example/springdatajpa/exception/OurExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.example.springdatajpa.exception;
 
+import com.example.springdatajpa.exception.response.ErrorDto;
 import com.example.springdatajpa.exception.response.ValidationErrorResponse;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Path;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -13,7 +15,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 @ControllerAdvice
-public class ValidationExceptionHandler {
+public class OurExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ValidationErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
@@ -32,6 +34,9 @@ public class ValidationExceptionHandler {
     }
 
     // we can have multiple handlers for multiple exception types, and also be returning different responses
-//    @ExceptionHandler(DepartmentNotFound.class)
-//    public ResponseEntity<ErrorDto> handleDepartmentNotFound...
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorDto> handleHttpMedialTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        ErrorDto errorDto = new ErrorDto("Provided data type is not acceptable for the endpoint: " + e.getMessage());
+        return new ResponseEntity<>(errorDto, HttpStatus.UNSUPPORTED_MEDIA_TYPE);
+    }
 }
--- src/main/java/com/example/springdatajpa/exception/response/ErrorDto.java
+++ src/main/java/com/example/springdatajpa/exception/response/ErrorDto.java
@@ -1,5 +1,10 @@
 package com.example.springdatajpa.exception.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ErrorDto {
     private String message;
 }
